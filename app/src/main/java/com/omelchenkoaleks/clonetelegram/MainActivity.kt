1246cd376b31cbe46b98355ec268930ccf99b9ec
Omelchenko Aleksandr <omelchenkoaleks@gmail.com>
2020-08-03 16:39:12 +0300

    Как привязать ViewBinding в Activity?

--- app/src/main/java/com/omelchenkoaleks/clonetelegram/MainActivity.kt
+++ app/src/main/java/com/omelchenkoaleks/clonetelegram/MainActivity.kt
@@ -2,14 +2,21 @@ package com.omelchenkoaleks.clonetelegram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import com.mikepenz.materialdrawer.AccountHeader
+import com.mikepenz.materialdrawer.Drawer
 import com.omelchenkoaleks.clonetelegram.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mBinding: ActivityMainBinding
+    private lateinit var mDrawer: Drawer
+    private lateinit var mHeader: AccountHeader
+    private lateinit var mToolbar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
     }
 }
