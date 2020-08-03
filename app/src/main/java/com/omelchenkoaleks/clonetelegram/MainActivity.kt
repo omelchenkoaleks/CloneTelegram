32fcd3d5ac9de4facaff8ec3c6ecd1779b558b10
Omelchenko Aleksandr <omelchenkoaleks@gmail.com>
2020-08-03 16:01:16 +0300

    Подключаем к проекту ViewBinding. Добавляем зависимости - одна из которых, очень хорошая библиотека MaterialDrawer.

--- app/src/main/java/com/omelchenkoaleks/clonetelegram/MainActivity.kt
+++ app/src/main/java/com/omelchenkoaleks/clonetelegram/MainActivity.kt
@@ -2,8 +2,12 @@ package com.omelchenkoaleks.clonetelegram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.omelchenkoaleks.clonetelegram.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
