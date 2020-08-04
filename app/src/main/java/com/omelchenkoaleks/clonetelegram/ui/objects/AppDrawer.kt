5a55e2758acedf592f6d769267ef03c97544d9d3
Omelchenko Aleksandr <omelchenkoaleks@gmail.com>
2020-08-04 09:54:18 +0300

    Refactoring. Активити не должна содержать функциональность - она просто запускает, включает и управляет - создавать что-то не должна. Вынес логику по работе Drawer в отдельный класс.

---
+++ app/src/main/java/com/omelchenkoaleks/clonetelegram/ui/objects/AppDrawer.kt
@@ -0,0 +1,115 @@
+package com.omelchenkoaleks.clonetelegram.ui.objects
+
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import com.mikepenz.materialdrawer.AccountHeader
+import com.mikepenz.materialdrawer.AccountHeaderBuilder
+import com.mikepenz.materialdrawer.Drawer
+import com.mikepenz.materialdrawer.DrawerBuilder
+import com.mikepenz.materialdrawer.model.DividerDrawerItem
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem
+import com.omelchenkoaleks.clonetelegram.R
+import com.omelchenkoaleks.clonetelegram.ui.SettingsFragment
+
+/*
+    В параметрах класса нужно получить привязки - это ведь пока просто класс, и он
+    ни к чему не привязан. Значит ему надо дать ссылки к чему ему нужно привязаться.
+    Нам при создании этого объекта нужно привязаться к нужной Активити и Toolbar.
+ */
+class AppDrawer(private val mainActivity: AppCompatActivity, private val toolbar: Toolbar) {
+    private lateinit var mDrawer: Drawer
+    private lateinit var mHeader: AccountHeader
+
+    /*
+        Функция будет доступна в Активити, и она будет создавать Header and Drawer.
+     */
+    fun create() {
+        createHeader()
+        createDrawer()
+    }
+
+    private fun createDrawer() {
+        mDrawer = DrawerBuilder()
+            .withActivity(mainActivity)
+            .withToolbar(toolbar)
+            .withActionBarDrawerToggle(true)
+            .withSelectedItem(-1)
+            .withAccountHeader(mHeader)
+            .addDrawerItems(
+                PrimaryDrawerItem().withIdentifier(101)
+                    .withIconTintingEnabled(true)
+                    .withName("Создать группу")
+                    .withSelectable(false)
+                    .withIcon(R.drawable.ic_menu_create_groups),
+                PrimaryDrawerItem().withIdentifier(102)
+                    .withIconTintingEnabled(true)
+                    .withName("Создать секретный чат")
+                    .withSelectable(false)
+                    .withIcon(R.drawable.ic_menu_secret_chat),
+                PrimaryDrawerItem().withIdentifier(103)
+                    .withIconTintingEnabled(true)
+                    .withName("Создать канал")
+                    .withSelectable(false)
+                    .withIcon(R.drawable.ic_menu_create_channel),
+                PrimaryDrawerItem().withIdentifier(104)
+                    .withIconTintingEnabled(true)
+                    .withName("Контакты")
+                    .withSelectable(false)
+                    .withIcon(R.drawable.ic_menu_contacts),
+                PrimaryDrawerItem().withIdentifier(105)
+                    .withIconTintingEnabled(true)
+                    .withName("Звонки")
+                    .withSelectable(false)
+                    .withIcon(R.drawable.ic_menu_phone),
+                PrimaryDrawerItem().withIdentifier(106)
+                    .withIconTintingEnabled(true)
+                    .withName("Избранное")
+                    .withSelectable(false)
+                    .withIcon(R.drawable.ic_menu_favorites),
+                PrimaryDrawerItem().withIdentifier(107)
+                    .withIconTintingEnabled(true)
+                    .withName("Настройки")
+                    .withSelectable(false)
+                    .withIcon(R.drawable.ic_menu_settings),
+                DividerDrawerItem(),
+                PrimaryDrawerItem().withIdentifier(108)
+                    .withIconTintingEnabled(true)
+                    .withName("Пригласить друзей")
+                    .withSelectable(false)
+                    .withIcon(R.drawable.ic_menu_invate),
+                PrimaryDrawerItem().withIdentifier(109)
+                    .withIconTintingEnabled(true)
+                    .withName("Вопросы о Телеграм")
+                    .withSelectable(false)
+                    .withIcon(R.drawable.ic_menu_help)
+            ).withOnDrawerItemClickListener(object : Drawer.OnDrawerItemClickListener{
+                override fun onItemClick(
+                    view: View?,
+                    position: Int,
+                    drawerItem: IDrawerItem<*>
+                ): Boolean {
+                    when (position) {
+                        7 -> mainActivity.supportFragmentManager.beginTransaction()
+                            .addToBackStack(null) // метод, нужен чтобы вернуться назад при нажатии на кнопку по стеку
+                            .replace(R.id.data_container, SettingsFragment()).commit()
+                    }
+                    return false
+                }
+            }).build()
+    }
+
+    private fun createHeader() {
+        mHeader = AccountHeaderBuilder()
+            .withActivity(mainActivity)
+            .withHeaderBackground(R.drawable.header)
+            .addProfiles(
+                ProfileDrawerItem().withName("Александр Омельченко")
+                    .withEmail("omelchenkoaleks@gmail.com")
+            ).build()
+    }
+
+
+}
