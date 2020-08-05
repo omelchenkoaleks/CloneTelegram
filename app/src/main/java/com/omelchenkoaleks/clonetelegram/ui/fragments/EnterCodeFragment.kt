104f8b59c7fb224b85b40cdb061734a7d08ab919
Omelchenko Aleksandr <omelchenkoaleks@gmail.com>
2020-08-05 10:56:49 +0300

    addTextChangedListener позволяет прослушивать изменения в EditText.

--- app/src/main/java/com/omelchenkoaleks/clonetelegram/ui/fragments/EnterCodeFragment.kt
+++ app/src/main/java/com/omelchenkoaleks/clonetelegram/ui/fragments/EnterCodeFragment.kt
@@ -1,11 +1,36 @@
 package com.omelchenkoaleks.clonetelegram.ui.fragments
 
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Toast
 import com.omelchenkoaleks.clonetelegram.R
+import kotlinx.android.synthetic.main.fragment_enter_code.*
 
 class EnterCodeFragment : BaseFragment(R.layout.fragment_enter_code) {
 
-    override fun onResume() {
-        super.onResume()
+    override fun onStart() {
+        super.onStart()
+        register_inter_code_edit_text.addTextChangedListener(object : TextWatcher{
+            override fun afterTextChanged(s: Editable?) {
+                val code = register_inter_code_edit_text.text.toString()
+                if (code.length == 6 ) {
+                    verificationCode()
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                TODO("Not yet implemented")
+            }
+
+        })
+    }
+
+    fun verificationCode() {
+        Toast.makeText(activity, "OK", Toast.LENGTH_SHORT).show()
     }
 
 }
