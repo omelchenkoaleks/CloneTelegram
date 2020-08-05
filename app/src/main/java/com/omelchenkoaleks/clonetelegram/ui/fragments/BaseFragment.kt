a4435b3bde6123e22024ef9b8718e33d770486ab
Omelchenko Aleksandr <omelchenkoaleks@gmail.com>
2020-08-05 19:53:44 +0300

    ViewRoot созданный в BaseFragment удаляем - при передаче во фрагмент layout свойства - синтетик сделает то же самое. ???

--- app/src/main/java/com/omelchenkoaleks/clonetelegram/ui/fragments/BaseFragment.kt
+++ app/src/main/java/com/omelchenkoaleks/clonetelegram/ui/fragments/BaseFragment.kt
@@ -1,28 +1,11 @@
 package com.omelchenkoaleks.clonetelegram.ui.fragments
 
-import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import com.omelchenkoaleks.clonetelegram.R
 
-open class BaseFragment(private val layout: Int) : Fragment() {
+open class BaseFragment(private val layout: Int) : Fragment(layout) {
 
-    private lateinit var mRootView: View
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        mRootView = inflater.inflate(layout, container, false)
-        return mRootView
-    }
-
-    /*
-        В этом методе будет происходить вся инициализация фрагмента.
-     */
     override fun onStart() {
         super.onStart()
     }
+
 }
