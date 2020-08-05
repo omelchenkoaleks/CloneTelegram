4ce5110e093ecd5f95247b7422b19df1e3731495
Omelchenko Aleksandr <omelchenkoaleks@gmail.com>
2020-08-05 19:06:48 +0300

    Refactoring. Создаем класс AppTextWatcher (наследник TextWatcher), свойство которого имеет функциональный тип. В месте применения используется лямбда, в которой по умолчанию вызывается эта функция (it). Сократил много лишнего кода.

--- app/src/main/java/com/omelchenkoaleks/clonetelegram/ui/fragments/EnterCodeFragment.kt
+++ app/src/main/java/com/omelchenkoaleks/clonetelegram/ui/fragments/EnterCodeFragment.kt
@@ -1,35 +1,23 @@
 package com.omelchenkoaleks.clonetelegram.ui.fragments
 
-import android.text.Editable
-import android.text.TextWatcher
 import android.widget.Toast
 import com.omelchenkoaleks.clonetelegram.R
+import com.omelchenkoaleks.clonetelegram.utils.AppTextWatcher
 import kotlinx.android.synthetic.main.fragment_enter_code.*
 
 class EnterCodeFragment : BaseFragment(R.layout.fragment_enter_code) {
 
     override fun onStart() {
         super.onStart()
-        register_inter_code_edit_text.addTextChangedListener(object : TextWatcher{
-            override fun afterTextChanged(s: Editable?) {
-                val code = register_inter_code_edit_text.text.toString()
-                if (code.length == 6 ) {
-                    verificationCode()
-                }
+        register_inter_code_edit_text.addTextChangedListener(AppTextWatcher {
+            val code = register_inter_code_edit_text.text.toString()
+            if (code.length == 6) {
+                verificationCode()
             }
-
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-
-            }
-
         })
     }
 
-    fun verificationCode() {
+    private fun verificationCode() {
         Toast.makeText(activity, "OK", Toast.LENGTH_SHORT).show()
     }
 
