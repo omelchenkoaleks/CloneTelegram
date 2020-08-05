535dab23928d0dcfcf47c8079bbd3f6536691826
Omelchenko Aleksandr <omelchenkoaleks@gmail.com>
2020-08-05 10:41:11 +0300

    RegisterActivity при старте запускаем EnterPhoneNumberFragment(). Во фрагменте проверяем если номер есть - замещаем на EnterCodeFragment().

--- app/src/main/java/com/omelchenkoaleks/clonetelegram/activities/RegisterActivity.kt
+++ app/src/main/java/com/omelchenkoaleks/clonetelegram/activities/RegisterActivity.kt
@@ -2,18 +2,32 @@ package com.omelchenkoaleks.clonetelegram.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import com.omelchenkoaleks.clonetelegram.R
 import com.omelchenkoaleks.clonetelegram.databinding.ActivityRegisterBinding
+import com.omelchenkoaleks.clonetelegram.ui.fragments.EnterPhoneNumberFragment
 
 /*
     Задача этой Activity произвести авторизацию или зарегистрировать нового пользогвателя.
  */
 class RegisterActivity : AppCompatActivity() {
 
-   private lateinit var mBinding: ActivityRegisterBinding
+    private lateinit var mBinding: ActivityRegisterBinding
+    private lateinit var mToolbar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mToolbar = mBinding.registerToolbar
+        setSupportActionBar(mToolbar)
+        title = getString(R.string.register_title_your_phone)
+        supportFragmentManager.beginTransaction()
+            .add(R.id.register_data_container, EnterPhoneNumberFragment())
+            .commit()
     }
 }
--- app/src/main/java/com/omelchenkoaleks/clonetelegram/ui/fragments/EnterPhoneNumberFragment.kt
+++ app/src/main/java/com/omelchenkoaleks/clonetelegram/ui/fragments/EnterPhoneNumberFragment.kt
@@ -1,9 +1,25 @@
 package com.omelchenkoaleks.clonetelegram.ui.fragments
 
+import android.widget.Toast
 import com.omelchenkoaleks.clonetelegram.R
+import kotlinx.android.synthetic.main.fragment_enter_phone_number.*
 
 class EnterPhoneNumberFragment : BaseFragment(R.layout.fragment_enter_phone_number) {
-    override fun onResume() {
-        super.onResume()
+
+    override fun onStart() {
+        super.onStart()
+        register_button_next_fab.setOnClickListener { sendCode() }
     }
+
+    private fun sendCode() {
+        if (register_input_phone_number_edit_text.text.toString().isEmpty()) {
+            Toast.makeText(activity, getString(R.string.register_toast_enter_phone), Toast.LENGTH_SHORT).show()
+        } else {
+            fragmentManager?.beginTransaction()
+                ?.replace(R.id.register_data_container, EnterCodeFragment())
+                ?.addToBackStack(null)
+                ?.commit()
+        }
+    }
+
 }
