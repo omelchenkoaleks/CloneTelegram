2646eb3d9edb4fb69db15187c8eb3ef6a3ba4970
Omelchenko Aleksandr <omelchenkoaleks@gmail.com>
2020-08-09 15:29:40 +0300

    Exit. Выходим из авторизованного аккаунта на активити регистрации/авторизации.

--- app/src/main/java/com/omelchenkoaleks/clonetelegram/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/omelchenkoaleks/clonetelegram/ui/fragments/SettingsFragment.kt
@@ -2,7 +2,12 @@ package com.omelchenkoaleks.clonetelegram.ui.fragments
 
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.MenuItem
+import com.omelchenkoaleks.clonetelegram.MainActivity
 import com.omelchenkoaleks.clonetelegram.R
+import com.omelchenkoaleks.clonetelegram.activities.RegisterActivity
+import com.omelchenkoaleks.clonetelegram.replaceActivity
+import com.omelchenkoaleks.clonetelegram.utils.AUTH
 
 class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
 
@@ -13,5 +18,18 @@ class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         activity?.menuInflater?.inflate(R.menu.settings_action_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.settings_menu_exit -> {
+                AUTH.signOut() // Выходим из авторизованного аккаунта
+                /*
+                    Теперь нужно обязательно запустить снова окно авторизации:
+                 */
+                (activity as MainActivity).replaceActivity(RegisterActivity())
+            }
+        }
+        return true
     }
 }
