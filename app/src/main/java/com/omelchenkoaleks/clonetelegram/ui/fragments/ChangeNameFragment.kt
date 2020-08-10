8300b1a35d903c431924b4c828c359e9a7573e51
Omelchenko Aleksandr <omelchenkoaleks@gmail.com>
2020-08-10 09:21:01 +0300

    Настройки меню settings_menu_confirm (создание и подключение).

--- app/src/main/java/com/omelchenkoaleks/clonetelegram/ui/fragments/ChangeNameFragment.kt
+++ app/src/main/java/com/omelchenkoaleks/clonetelegram/ui/fragments/ChangeNameFragment.kt
@@ -1,16 +1,32 @@
 package com.omelchenkoaleks.clonetelegram.ui.fragments
 
-import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import com.omelchenkoaleks.clonetelegram.MainActivity
 import com.omelchenkoaleks.clonetelegram.R
 
 class ChangeNameFragment : Fragment(R.layout.fragment_change_name) {
 
     override fun onResume() {
         super.onResume()
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        (activity as MainActivity).menuInflater.inflate(R.menu.settings_menu_confirm, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.settings_confirm_change -> changeName()
+        }
+        return true
+    }
+
+    private fun changeName() {
+        // Сначала при нажатии на кнопку мы должны считать данные.
     }
 
 }
