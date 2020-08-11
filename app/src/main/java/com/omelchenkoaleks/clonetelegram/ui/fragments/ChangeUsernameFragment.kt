a3611ae5927c0919c92c5b562a2abac2c94cc7d0
Omelchenko Aleksandr <omelchenkoaleks@gmail.com>
2020-08-11 09:34:58 +0300

    Подгружаем username в EditText, который на данный момент есть у пользователя.

--- app/src/main/java/com/omelchenkoaleks/clonetelegram/ui/fragments/ChangeUsernameFragment.kt
+++ app/src/main/java/com/omelchenkoaleks/clonetelegram/ui/fragments/ChangeUsernameFragment.kt
@@ -5,6 +5,8 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import com.omelchenkoaleks.clonetelegram.MainActivity
 import com.omelchenkoaleks.clonetelegram.R
+import com.omelchenkoaleks.clonetelegram.utils.USER
+import kotlinx.android.synthetic.main.fragment_change_username.*
 
 class ChangeUsernameFragment : BaseFragment(R.layout.fragment_change_username) {
 
@@ -13,6 +15,8 @@ class ChangeUsernameFragment : BaseFragment(R.layout.fragment_change_username) {
     override fun onResume() {
         super.onResume()
         setHasOptionsMenu(true)
+        // Подгружаем тот username что есть на данный момент.
+        settings_input_username.setText(USER.username)
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
