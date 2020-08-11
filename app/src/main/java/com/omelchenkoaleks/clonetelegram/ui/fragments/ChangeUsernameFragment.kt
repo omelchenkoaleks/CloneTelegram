1411cbab542f5d1a2e7400fdeae410aca87f515a
Omelchenko Aleksandr <omelchenkoaleks@gmail.com>
2020-08-11 12:24:57 +0300

    NODE_USERNAMES - новый (node) узел в базе данных. Благодаря ему будем конролировать добавление уникальных username. При установке от пользователя username - обновляем его данные (username) по UID.

--- app/src/main/java/com/omelchenkoaleks/clonetelegram/ui/fragments/ChangeUsernameFragment.kt
+++ app/src/main/java/com/omelchenkoaleks/clonetelegram/ui/fragments/ChangeUsernameFragment.kt
@@ -1,12 +1,15 @@
 package com.omelchenkoaleks.clonetelegram.ui.fragments
 
+import android.graphics.RectF
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.widget.Toast
 import com.omelchenkoaleks.clonetelegram.MainActivity
 import com.omelchenkoaleks.clonetelegram.R
-import com.omelchenkoaleks.clonetelegram.utils.USER
+import com.omelchenkoaleks.clonetelegram.utils.*
 import kotlinx.android.synthetic.main.fragment_change_username.*
+import java.util.*
 
 class ChangeUsernameFragment : BaseFragment(R.layout.fragment_change_username) {
 
@@ -31,6 +34,33 @@ class ChangeUsernameFragment : BaseFragment(R.layout.fragment_change_username) {
     }
 
     private fun change() {
-        TODO("Not yet implemented")
+        mNewUsername = settings_input_username.text.toString().toLowerCase(Locale.getDefault())
+        if (mNewUsername.isEmpty()) {
+            showToast("Поле пустое")
+        } else {
+            changeUsername()
+        }
+    }
+
+    private fun changeUsername() {
+        // Новый username прописываем в базу данных вместе с сылкой на id пользователя.
+        REF_DATABASE_ROOT.child(NODE_USERNAMES).child(mNewUsername).setValue(UID)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    updateCurrentUsername() // Нужно обновить в базе также на новый username у пользователя.
+                }
+            }
+    }
+
+    private fun updateCurrentUsername() {
+        REF_DATABASE_ROOT.child(NODE_USERS).child(UID).child(CHILD_USERNAME)
+            .setValue(mNewUsername)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    showToast(getString(R.string.toast_data_update))
+                } else {
+                    showToast(it.exception?.message.toString()  )
+                }
+            }
     }
 }
--- app/src/main/java/com/omelchenkoaleks/clonetelegram/utils/FirebaseHelper.kt
+++ app/src/main/java/com/omelchenkoaleks/clonetelegram/utils/FirebaseHelper.kt
@@ -17,6 +17,8 @@ lateinit var USER: User
 
 
 const val NODE_USERS = "users"
+const val NODE_USERNAMES = "usernames"
+
 const val CHILD_ID = "id"
 const val CHILD_PHONE = "phone"
 const val CHILD_USERNAME = "username"
