673b8e672d11806a99bc66eb02d26f59d7494254
Omelchenko Aleksandr <omelchenkoaleks@gmail.com>
2020-08-18 09:23:07 +0300

    FirebaseRecyclerView. Как с ним работать...

--- app/src/main/java/com/omelchenkoaleks/clonetelegram/ui/fragments/ContactsFragment.kt
+++ app/src/main/java/com/omelchenkoaleks/clonetelegram/ui/fragments/ContactsFragment.kt
@@ -1,13 +1,72 @@
 package com.omelchenkoaleks.clonetelegram.ui.fragments
 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.firebase.ui.database.FirebaseRecyclerAdapter
+import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.google.firebase.database.DatabaseReference
 import com.omelchenkoaleks.clonetelegram.R
-import com.omelchenkoaleks.clonetelegram.utils.APP_ACTIVITY
+import com.omelchenkoaleks.clonetelegram.models.CommonModel
+import com.omelchenkoaleks.clonetelegram.utils.*
+import de.hdodenhof.circleimageview.CircleImageView
+import kotlinx.android.synthetic.main.contact_item.view.*
+import kotlinx.android.synthetic.main.fragment_contacts.*
 
 class ContactsFragment : BaseFragment(R.layout.fragment_contacts) {
+
+    private lateinit var mRecyclerView: RecyclerView
+    private lateinit var mAdapter: FirebaseRecyclerAdapter<CommonModel, ContactsHolder>
+    private lateinit var mRefContacts: DatabaseReference // ссылка откуда мы будем скачивать наши данные
 
     override fun onResume() {
         super.onResume()
         APP_ACTIVITY.title = "Контакты"
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        mRecyclerView = contacts_recycler_view
+        mRefContacts = REF_DATABASE_ROOT.child(NODE_PHONES_CONTACTS).child(CURRENT_UID)
+
+        val options = FirebaseRecyclerOptions.Builder<CommonModel>()
+            .setQuery(mRefContacts, CommonModel::class.java)
+            .build()
+
+        mAdapter = object : FirebaseRecyclerAdapter<CommonModel, ContactsHolder>(options) {
+            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactsHolder {
+                val view = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.contact_item, parent, false)
+                return ContactsHolder(view)
+            }
+
+            override fun onBindViewHolder(
+                holder: ContactsHolder,
+                position: Int,
+                model: CommonModel
+            ) {
+                holder.name.text = model.fullName
+                holder.status.text = model.state
+                holder.photo.downloadAndSetImage(model.photoUrl)
+            }
+        }
+
+        mRecyclerView.adapter = mAdapter
+        mAdapter.startListening()
+
+    }
+
+    class ContactsHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val name: TextView = view.contact_fullname
+        val status: TextView = view.contact_status
+        val photo: CircleImageView = view.contact_photo
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mAdapter.stopListening()
     }
 
 }
