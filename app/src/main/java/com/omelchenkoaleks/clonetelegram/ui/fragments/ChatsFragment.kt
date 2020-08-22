3585b9ab37bbc5b12b56a0a025b89f4c748676ab
Omelchenko Aleksandr <omelchenkoaleks@gmail.com>
2020-08-22 08:13:18 +0300

    title - "Чаты"

--- app/src/main/java/com/omelchenkoaleks/clonetelegram/ui/fragments/ChatsFragment.kt
+++ app/src/main/java/com/omelchenkoaleks/clonetelegram/ui/fragments/ChatsFragment.kt
@@ -2,6 +2,7 @@ package com.omelchenkoaleks.clonetelegram.ui.fragments
 
 import androidx.fragment.app.Fragment
 import com.omelchenkoaleks.clonetelegram.R
+import com.omelchenkoaleks.clonetelegram.utils.APP_ACTIVITY
 
 /*
     Главный экран приложения.
@@ -12,6 +13,6 @@ class ChatsFragment : Fragment(R.layout.fragment_chats) {
 
     override fun onResume() {
         super.onResume()
-
+        APP_ACTIVITY.title = "Чаты"
     }
 }
