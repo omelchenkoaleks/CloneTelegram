07fe9d82d5c4d647eee1f24036586f360a797e45
Omelchenko Aleksandr <omelchenkoaleks@gmail.com>
2020-09-09 15:38:35 +0300

    Переопределил метод equals() на сравнение по id. Теперь два элемента мы сравниваем по id, а не timestamp.

--- app/src/main/java/com/omelchenkoaleks/clonetelegram/database/appDatabaseHelper.kt
+++ app/src/main/java/com/omelchenkoaleks/clonetelegram/database/appDatabaseHelper.kt
@@ -133,6 +133,7 @@ fun sendMessage(message: String, receivingUserId: String, typeText: String, func
     mapMessage[CHILD_FROM] = CURRENT_UID
     mapMessage[CHILD_TYPE] = typeText
     mapMessage[CHILD_TEXT] = message
+    mapMessage[CHILD_ID] = messageKey.toString()
     mapMessage[CHILD_TIME_STAMP] = ServerValue.TIMESTAMP
 
     val mapDialog = hashMapOf<String, Any>()
